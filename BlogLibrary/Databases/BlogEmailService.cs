14d69f40b9e7272c4a13c3d0d370d55b77d2c7a2
Lynda Foster <lfost42@wgu.edu>
2022-04-17 15:55:48 -0700

    debugged contact form

--- BlogLibrary/Databases/BlogEmailService.cs
+++ BlogLibrary/Databases/BlogEmailService.cs
@@ -20,9 +20,26 @@ namespace BlogLibrary.Databases
             _emailSettings = emailSettings.Value;
         }
 
-        public async Task SendContactEmailAsync(string emailFrom, string name, string subject, string message)
+        public async Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_emailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(_emailSettings.Mail));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = $"<b>{name}</b> has sent you an email from the MyBlog website.<br/><br/>{htmlMessage}";
+
+
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_emailSettings.Host, _emailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_emailSettings.Mail, _emailSettings.Password);
+
+            await smtp.SendAsync(email);
+
+            smtp.Disconnect(true);
         }
 
         public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
