fa5d4c23d5f755ac08916297debabbad474ec4ad
Lynda Foster <lfost42@wgu.edu>
2022-10-10 18:27:56 -0700

    docker trial

--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM mcr.microsoft.com/dotnet/aspnet:5.0-focal AS base
-WORKDIR /app
+WORKDIR /BrailleAPI
 EXPOSE 80
 EXPOSE 443
 
@@ -10,12 +10,12 @@ COPY ["Blog.Data/Blog.Data.csproj", "Braille.Data/"]
 RUN dotnet restore "Blog/Blog.csproj"
 COPY . .
 WORKDIR "/src/Blog"
-RUN dotnet build "Blog.csproj" -c Release -o /app
+RUN dotnet build "Blog.csproj" -c Release -o /BrailleAPI
 
 FROM build AS publish
-RUN dotnet publish "Blog.csproj" -c Release -o /app
+RUN dotnet publish "Blog.csproj" -c Release -o /BrailleAPI
 
 FROM base AS final
-WORKDIR /app
-COPY --from=publish /app .
+WORKDIR /BrailleAPI
+COPY --from=publish /BrailleAPI .
 ENTRYPOINT ["dotnet", "Blog.Data.dll"]
---
+++ out/wwwroot/js/site.js
@@ -0,0 +1,78 @@
+﻿let index = 0;
+
+const AddTag = () => {
+    var tagEntry = document.getElementById("TagEntry");
+    let searchResult = search(tagEntry.value);
+    if (searchResult) {
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR!',
+            text: searchResult,
+            confirmButtonColor: '#212422',
+            width: 300,
+            timer: 3000,
+            timerProgressBar: true,
+        })
+        tagEntry.value = "";
+    } else {
+        let newOption = new Option(tagEntry.value, tagEntry.value);
+        document.getElementById("TagList").options[index++] = newOption;
+        tagEntry.value = "";
+        return true;
+    }
+}
+
+const DeleteTag = () => {
+    let tagCount = 1;
+    let tagList = document.getElementById("TagList");
+    if (!tagList) return false;
+
+    if (tagList.selectedIndex == -1) {
+        Swal.fire({
+            icon: 'error',
+            title: 'error',
+            text: 'Please choose a tag to delete!',
+            confirmButtonColor: '#212422',
+            width: 300,
+            timer: 3000,
+            timerProgressBar: true,
+        });
+        return true;
+    }
+    while (tagCount > 0) {
+        if (tagList.selectedIndex >= 0) {
+            tagList.options[tagList.selectedIndex] = null;
+            tagCount--;
+        } else
+            tagCount = 0;
+        index--;
+    }
+}
+
+$("form").on("submit", () => {
+    $("#TagList option").prop("selected", "selected");
+})
+
+if (tagValues != "" || tagValues != null) {
+    let tagArray = tagValues.split(",");
+    for (let i = 0; i < tagArray.length; i++) {
+        ReplaceTag(tagArray[i], i);
+        index++;
+    }
+}
+
+const ReplaceTag = (tag, index) => {
+    let newOption = new Option(tag, tag);
+    document.getElementById("TagList").options[index] = newOption;
+}
+
+function search(str) {
+    if (str == "") return "Please enter a non-empty tag.";
+    var tags = document.getElementById("TagList");
+    if (tags) {
+        let options = tags.options;
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].value == str) return `#${str} is a duplicate.`;
+        }
+    }
+}
